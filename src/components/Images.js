fe1cfe8386f1f704e812ba5ea03172ebd9d68f01
sharkinsspatial <sharkinsgis@gmail.com>
2015-12-15 18:49:42 -0300

    Fix date parsing issue.

--- src/components/Images.js
+++ src/components/Images.js
@@ -5,8 +5,10 @@ import moment from 'moment'
 var Images = React.createClass({
     render() {
         let imageItems = this.props.images.map(item => {
-            let formatDate = moment(item.collectionDate).format('MMMM Do YYYY')
-            return <option key={item.id} value={item.id}>{formatDate}</option>
+            //Removes UTC Z time code from string.
+            let dateNoTime = item.collectionDate.split('T')[0]
+            let formatDate = moment(dateNoTime).format('MMMM Do YYYY')
+            return <option key={item.id} value={item}>{formatDate}</option>
         })
         return (
             <div>
